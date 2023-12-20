6a045264c4f3376388d82a2ba3e16e5f0c62027c
IkaroJS <ikarojesusdasilva@outlook.com>
2023-12-20 09:21:53 -0300

    Listagem de professores finalizada

--- professores.php
+++ professores.php
@@ -3,9 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <title>Lista de professores</title>
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap" rel="stylesheet">
     
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>    
+    <?php include "conexao_bd.php"; 
+    
+    $sql = "SELECT * FROM professor";
+
+    $resultado = retornarDados($sql);
+    ?>
+
+    <form name="" action="" method="post">
+        <div class="row g-3">
+            <div class="col-sm-6">
+                <div class="form-floating">
+                    <h2>Nossos professores</h2>
+                    <table>                        
+                        <tr>
+                            <td>Nome</td>
+                            <td>Formação</td>
+                            <td>Turno</td>
+                            <td>Tempo de experiência</td>
+                            <td>Escola anterior</td>
+                        </tr>
+
+                        <?php                             
+                            while ($linha = mysqli_fetch_assoc($resultado))
+                            {
+                                $nome = $linha["nome_professor"];
+                                $formacao = $linha["formacao"];
+                                $turno = $linha["turno"];
+                                $experiencia = $linha["tempo_experiencia"];
+                                $escola = $linha["escola_anterior"];                            
+                        ?>
+
+                        <tr>
+                            <td><?php echo $nome; ?></td>
+                            <td><?php echo $formacao; ?></td>
+                            <td><?php echo $turno; ?></td>
+                            <td><?php echo $experiencia; ?></td>
+                            <td><?php echo $escola; ?></td>                            
+                        </tr>
+
+                        <?php } ?>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
